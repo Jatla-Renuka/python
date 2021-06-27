3bd8fd6f5a60400cb37e1c2fbd1867e9ce20c4e7
Renuka <84368764+Jatla-Renuka@users.noreply.github.com>
2021-06-27 18:17:46 +0530

    Create recursive.py

---
+++ recursive.py
@@ -0,0 +1,18 @@
+n=int(input())
+f=int(input())
+c=0
+i=1
+a=f
+def fact(n,f,c,i):
+    if i>n:
+        return 0
+    if n%i==0:
+        c+=1
+        if c==f:
+            return i
+        else:
+            return fact(n,f,c,i+1)
+    else:
+        return fact(n,f,c,i+1)
+
+print(fact(n,f,c,i))        
